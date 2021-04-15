cd3dca920b732a2f106f51ff651a93488aad7d65
guillaumefontan <82604059+guillaumefontan@users.noreply.github.com>
2021-04-15 20:21:08 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,59 @@
+namespace Pokemon
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Pokemon.Configuration;
+    using Pokemon.Helpers;
+    using Pokemon.Repositories;
+    using Pokemon.Services;
+
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">Configuration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<PokemonConfiguration>(this.Configuration.GetSection(nameof(PokemonConfiguration)));
+
+            services.AddControllers();
+
+            services.AddHttpClient<IPokemonDataRepository, PokemonDataRepository>();
+            services.AddTransient<IPokemonDataService, PokemonDataService>();
+            services.AddTransient<IRequestBuilder, RequestBuilder>();
+            services.AddTransient<IPokemonDataParser, PokemonDataParser>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
